783a74e5294af521d8513feb2b040537ad003402
Brandon Jolley <brandonjolley97@gmail.com>
2015-10-19 09:54:29 -0600

    added a loop and changed some of the strings

--- src/formative/controller/SLOController.java
+++ src/formative/controller/SLOController.java
@@ -5,6 +5,7 @@ import formative.view.SLOPopup;
 public class SLOController 
 {
 	private SLOPopup myPopups;
+	double cookies = 10;
 	
 	public SLOController()
 	{
@@ -13,8 +14,14 @@ public class SLOController
 	
 	public void start()
 	{
-		double cookies;
-		cookies = 10;
+		for(int startCount = 0;startCount<10;startCount++)
+		{
+		runMouse();
+		}
+	}
+	
+	public void runMouse()
+	{
 		
 		int mouseTraps;
 		mouseTraps = 0;
@@ -22,56 +29,65 @@ public class SLOController
 		if(cookies == 9)
 		{
 			myPopups.grabAnswer("A mouse has eaten one of your cookies!  Do you want to put a mouse trap next to your cookies?");
-			myPopups.showResponse("Regardless of how you answered, a mouse trap was placed next to the cookies.");
 			mouseTraps = 1;
+			myPopups.showResponse("Regardless of how you answered, a mouse trap was placed next to the cookies.  There is " + mouseTraps + " mouse trap.");
+			
 		}
 		else if(cookies == 8)
 		{
 			myPopups.grabAnswer("A mouse has eaten another one of your cookies!  Do you want to put a another mouse trap next to your cookies?");
-			myPopups.showResponse("Regardless of how you answered, another mouse trap was placed next to the cookies.");
 			mouseTraps = 2;
+			myPopups.showResponse("Regardless of how you answered, another mouse trap was placed next to the cookies.  There are " + mouseTraps + " mouse traps.");
+			
 		}
 		else if(cookies == 7)
 		{
 			myPopups.grabAnswer("A mouse has eaten another one of your cookies!  Do you want to put a another mouse trap next to your cookies?");
-			myPopups.showResponse("Regardless of how you answered, another mouse trap was placed next to the cookies.");
 			mouseTraps = 3;
+			myPopups.showResponse("Regardless of how you answered, another mouse trap was placed next to the cookies.  There are " + mouseTraps + " mouse traps.");
+			
 		}
 		else if(cookies == 6)
 		{
 			myPopups.grabAnswer("A mouse has eaten another one of your cookies!  Do you want to put a another mouse trap next to your cookies?");
-			myPopups.showResponse("Regardless of how you answered, another mouse trap was placed next to the cookies.");
 			mouseTraps = 4;
+			myPopups.showResponse("Regardless of how you answered, another mouse trap was placed next to the cookies.  There are " + mouseTraps + " mouse traps.");
+			
 		}
 		else if(cookies == 5)
 		{
 			myPopups.grabAnswer("A mouse has eaten another one of your cookies!  Do you want to put a another mouse trap next to your cookies?");
-			myPopups.showResponse("Regardless of how you answered, another mouse trap was placed next to the cookies.");
 			mouseTraps = 5;
+			myPopups.showResponse("Regardless of how you answered, another mouse trap was placed next to the cookies.  There are " + mouseTraps + " mouse traps.");
+			
 		}
 		else if(cookies == 4)
 		{
 			myPopups.grabAnswer("A mouse has eaten another one of your cookies!  Do you want to put a another mouse trap next to your cookies?");
-			myPopups.showResponse("Regardless of how you answered, another mouse trap was placed next to the cookies.");
 			mouseTraps = 6;
+			myPopups.showResponse("Regardless of how you answered, another mouse trap was placed next to the cookies.  There are " + mouseTraps + " mouse traps.");
+			
 		}
 		else if(cookies == 3)
 		{
 			myPopups.grabAnswer("A mouse has eaten another one of your cookies!  Do you want to put a another mouse trap next to your cookies?");
-			myPopups.showResponse("Regardless of how you answered, another mouse trap was placed next to the cookies.");
 			mouseTraps = 7;
+			myPopups.showResponse("Regardless of how you answered, another mouse trap was placed next to the cookies.  There are " + mouseTraps + " mouse traps.");
+			
 		}
 		else if(cookies == 2)
 		{
 			myPopups.grabAnswer("A mouse has eaten another one of your cookies!  Do you want to put a another mouse trap next to your cookies?");
-			myPopups.showResponse("Regardless of how you answered, another mouse trap was placed next to the cookies.");
 			mouseTraps = 8;
+			myPopups.showResponse("Regardless of how you answered, another mouse trap was placed next to the cookies.  There are " + mouseTraps + " mouse traps.");
+			
 		}
 		else if(cookies == 1)
 		{
 			myPopups.grabAnswer("A mouse has eaten another one of your cookies!  Do you want to put a another mouse trap next to your cookies?");
-			myPopups.showResponse("Regardless of how you answered, another mouse trap was placed next to the cookies.");
 			mouseTraps = 9;
+			myPopups.showResponse("Regardless of how you answered, another mouse trap was placed next to the cookies.  There are " + mouseTraps + " mouse traps.");
+			
 		}
 		else if(cookies == 0)
 		{
